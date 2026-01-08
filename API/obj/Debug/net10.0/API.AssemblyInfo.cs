412b126f1b9be0b3fb182b3687546cbf95400cd5
Jonas Rishøj <jonas@gefiber.dk>
2026-01-08 12:54:04 +0100

    Ændringer

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebee39803e94dca683f2b948718b0921f4f8fc76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3537a62e71693ef4753d152919695c6798a9bf26")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
